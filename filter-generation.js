a0e83ca6fea0f7e0f5cae7666f50ec2770a7dd17
Leonel Oliveira <leoneloliver@gmail.com>
2018-08-19 11:43:16 -0400

    update filter

--- filter-generation.js
+++ filter-generation.js
@@ -1,5 +1,3 @@
-const data = require('./data/queries.json');
-
 /**
  You will use data that we we using in production. You will need to generate unique filters from
  a list of data.
@@ -92,4 +90,68 @@ const data = require('./data/queries.json');
 /**
  * Answer
  */
+
+const data = require('./data/queries.json');
+
+let attr = [];
+let categ = [];
+let stat = [];
+let creatr = [];
+const typeOfData = 'CATEGORY';
+
+data.forEach((element, index) => {
+  //The `attribute` and `category` values will need to be pulled out of the `annotations` field value and aggregated based on `type`
+  //separating attributes from categories
+  if(data[index].annotations.length !== 0){
+    //getting category here
+    if(data[index].annotations[0].value !== "" && data[index].annotations[0].type === typeOfData){
+      categ.push(data[index].annotations[0].name);
+    //getting attributes here  
+    }else{
+      attr.push(data[index].annotations[0].name + ':' + data[index].annotations[0].value);
+    }
+  }
+  // getting status
+  if(data[index].status.length !== 0){
+    if(data[index].status !== null){
+      stat.push(data[index].status);
+    }
+  }
+  // getting creators
+  if(data[index].metadata.length !== 0){
+    if (data[index].metadata.created_by !== null){
+      creatr.push(data[index].metadata.created_by);
+    }
+  }
+});
+
+//All lists ( `attribute`, `category`, `status` and `creator` ) should be unique with no falsey values
+//removing duplicates results
+attr   = [...new Set(attr)];
+categ  = [...new Set(categ)];
+stat   = [...new Set(stat)];
+creatr = [...new Set(creatr)];
+
+//`attribute`, `category` and `status` should be sorted alphabetically
+// sorting results 
+attr.sort();
+categ.sort();
+stat.sort();
+creatr.sort();
+
+// 
+console.log('attribute: [');
+console.log(attr);
+console.log('],');
+console.log('');
+console.log('category:[');
+console.log(categ);
+console.log('],');
+console.log('status:[');
+console.log(stat);
+console.log('],');
+console.log('creator:[');
+console.log(creatr);
+console.log(']');
+
 
